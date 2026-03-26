6c04545bf968179ff4c10f349d0229cbe46be17c
Henry <henry.karppinen1997@gmail.com>
2026-03-26 17:45:06 +0200

    Started to add unit tests, added testing setup file

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
     globals: true,
-    
+    server: {
+      deps: {
+        inline: ["jsdom"],
+      },
+    }
   },
 })
