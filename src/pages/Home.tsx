d09246bedbda2313d7946de23cba14955e1f6e54
Henry <160464746+W0lffe@users.noreply.github.com>
2026-03-30 18:01:05 +0300

    Added home page (#24)

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+
+export default function Home(){
+
+    return(
+        <div>
+            <h2>Welcome to Subnex!</h2>
+            <p>Subnex is a subnet calculator that allows you to easily calculate subnet information for your IPv4 networks.</p>
+            <ul>
+                <li>Calculate network and broadcast addresses</li>
+                <li>Determine the number of available host addresses</li>
+                <li>Identify the subnet mask in dotted decimal and CIDR notation</li>
+                <li>Get wildcard mask</li>
+            </ul>
+            <p>To get started, click on the "Calculator" button in the navigation menu.</p>
+        </div>
+    )
+}
