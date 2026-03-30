f9a213012bccec09a78f57ed03d2dcd27226031e
Henry <henry.karppinen1997@gmail.com>
2026-03-30 18:57:26 +0300

    Started to apply styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import Navigation from '../Navigation/Navigation';
 
+const divStyle = "w-screen border-b border-gray-400/40 shadow-sm shadow-black/20 pt-2 px-4 flex justify-center items-end ";
+const spanStyle = "flex flex-row justify-between w-full md:w-1/2 h-full";
+const h1Style = "text-xl md:text-3xl font-semibold";
+
 export default function Header(){
+
     return(
-        <div className='flex flex-row '>
-            <h1>Subnex</h1>
-            <Navigation />
+        <div className={divStyle}>
+            <span className={spanStyle}>
+                <h1 className={h1Style}>Subnex</h1>
+                <Navigation />
+            </span>
         </div>
     )
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,10 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+const buttonStyle = "p-0 hover:px-2 hover:border-b-red-600 hover:border-b-2 rounded-[4px] hover:bg-linear-to-b hover:from-gray-200/10 hover:to-gray-400/25 transition-all duration-300 text-lg";
+const divStyle = "flex flex-row gap-3 h-full";
+
 export default function Navigation(){
 
     const navigateTo = useNavigate();
     const [isActive, setIsActive] = useState("/");
+
+    const getActiveButton = (path: string) => {
+        return isActive === path ? " border-b-red-600 border-b-2 font-semibold" : " border-b-red-600/0";
+    }
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const value = event.currentTarget.value;
@@ -14,9 +21,9 @@ export default function Navigation(){
     }
 
     return(
-        <div>
-            <button value="/" onClick={(e) => handleClick(e)}>Home</button>
-            <button value="/calc" onClick={(e) => handleClick(e)}>Calculator</button>
+        <div className={divStyle}>
+            <button value="/" onClick={(e) => handleClick(e)} className={`${buttonStyle} ${getActiveButton("/")}`}>Home</button>
+            <button value="/calc" onClick={(e) => handleClick(e)} className={`${buttonStyle} ${getActiveButton("/calc")}`}>Calculator</button>
         </div>
     )
 }
