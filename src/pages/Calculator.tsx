cae3f4b938a757a6875f7822980435e24849dd04
Henry <henry.karppinen1997@gmail.com>
2026-03-31 14:06:13 +0300

    fixed styling

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -3,7 +3,7 @@ import InputForm from '../components/InputForm/InputForm';
 import type { SubnetResult } from '../models/SubnetResult';
 import { useState } from 'react';
 
-const divStyle = 'w-screen md:w-1/2 h-screen flex flex-col items-center p-2 md:gap-5';
+const divStyle = 'w-screen md:w-1/2 h-fit md:h-screen flex flex-col items-center p-2 md:gap-5';
 
 export default function Calculator(){
 
